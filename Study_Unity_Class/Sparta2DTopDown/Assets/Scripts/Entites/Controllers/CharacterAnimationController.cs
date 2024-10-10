ab7f0f50cfecc0a693f2238490c5d20e2621e997
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-10 23:56:04 +0900

    내배캠 유니티 13강 애니메이션 컨트롤

---
+++ Study_Unity_Class/Sparta2DTopDown/Assets/Scripts/Entites/Controllers/CharacterAnimationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterAnimationController : AnimationController
+{
+    private static readonly int isWalking = Animator.StringToHash("isWalking");
+    private static readonly int isHit = Animator.StringToHash("isHit");
+    private static readonly int Attack = Animator.StringToHash("attack");
+
+    private readonly float magnituteThreshold = 0.5f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        controller.OnAttackEvent += Attacking;
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 vector)
+    {
+        animator.SetBool(isWalking, vector.magnitude > magnituteThreshold);
+    }
+
+    private void Attacking(AttackSO sO)
+    {
+        animator.SetTrigger(Attack);
+    }
+
+    private void Hit()
+    {
+        animator.SetBool(isHit, true);
+    }
+
+    private void InvincibilityEnd()
+    {
+        animator.SetBool(isHit, false);
+    }
+}
