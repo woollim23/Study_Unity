e6c19986bbe631c592a72a6d4474e7252ab8defd
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-14 10:19:31 +0900

    스파르타 타운(과제) - (도전) npc 대화창 구현

---
+++ Study_Unity_Class/SpartaTown/Assets/Scripts/Behavior/TalkDistance.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class TalkDistance : MonoBehaviour
+{
+    protected Transform ClosetTarget { get; private set; }
+    [SerializeField][Range(0f, 100f)] private float TalkRange;
+    public GameObject talkUI;
+    private bool buttonPop; 
+
+    protected void Start()
+    {
+        TalkRange = 3f;
+        buttonPop = true;
+    }
+
+    protected void FixedUpdate()
+    {
+        ClosetTarget = GameManager.instance.Player;
+        if (ClosetTarget)
+        {
+            //Debug.LogError(DistanceToTarget());
+            if (DistanceToTarget() < TalkRange)
+            {
+                talkUI.SetActive(buttonPop);
+            }
+            else
+            {
+                talkUI.SetActive(false);
+            }
+        }
+    }
+
+    protected float DistanceToTarget()
+    {
+        return Vector3.Distance(transform.position, ClosetTarget.position);
+    }
+
+    public void OffTalkButton()
+    {
+        buttonPop = false;
+    }
+
+    public void ONTalkButton()
+    {
+        buttonPop = true;
+    }
+}
--- Study_Unity_Class/SpartaTown/Assets/Scripts/GameManager.cs
+++ Study_Unity_Class/SpartaTown/Assets/Scripts/GameManager.cs
@@ -8,13 +8,17 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [SerializeField] private string playerTag;
     public Player player;
     public int playerId;
 
     public string playerName;
 
+    public Transform Player { get; private set; }
+
     private void Awake()
     {
+        if (instance != null) Destroy(gameObject);
         instance = this;
         playerId = 0;
     }
@@ -22,7 +26,7 @@ public class GameManager : MonoBehaviour
     public void GameStart()
     {
         player.gameObject.SetActive(true);
-        
+        Player = GameObject.FindGameObjectWithTag(playerTag).transform;
     }
 
     public void SetId(int id)
